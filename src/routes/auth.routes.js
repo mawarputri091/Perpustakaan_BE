8894c584734591ee26b245fb21328cb35a453028
NoLife1011 <ezfarelez1011@gmail.com>
2026-04-21 05:55:33 +0700

    feat: [OnProgress] fitur login
    menambahkan logic login, validasi user, dan generate token JWT

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,10 +3,31 @@ const router = express.Router()
 
 const authMiddleware = require('../middlewares/auth.middleware')
 const authController = require('../controllers/auth.controller')
+const authService = require('../services/auth.service')
 
 router.post('/login', authController.login)
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body
+
+    const token = await authService.login(username, password)
+
+    res.json({
+      message: "Login berhasil",
+      token
+    })
+  } catch (err) {
+    res.status(err.statusCode || 500).json({
+      message: err.message
+    })
+  }
+}
 
 // route TERPROTEKSI
 router.post('/logout', authMiddleware, authController.logout)
 
+
 module.exports = router
+
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const app = require('./app')
 
+const authRoutes = require('./routes/auth.routes')
+app.use('/auth', authRoutes)
+
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
